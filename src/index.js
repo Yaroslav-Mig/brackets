2338075897693fb1add34c5d77c62049f96950cf
Yaroslav Mescheryakov <migyaroslav@gmail.com>
2019-10-13 02:33:35 +0300

    feat: implement brackets-task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let tempStr = str;
+
+    for (let strIndex = tempStr.length; strIndex >= 0; strIndex--) {
+        for (let bracketIndex = 0; bracketIndex < bracketsConfig.length; bracketIndex++) {
+            if (tempStr[strIndex] === bracketsConfig[bracketIndex][0]) {
+                tempStr = tempStr.replace(bracketsConfig[bracketIndex].join(''), '');
+            }
+        }
+    }
+
+    return tempStr.length === 0;
+};
