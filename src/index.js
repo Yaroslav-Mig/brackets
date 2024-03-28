69ead9bb58f5d56e791892010f12771b64d8ba0c
Yaroslav Mescheryakov <33201546+Yaroslav-Mig@users.noreply.github.com>
2024-03-28 15:35:39 +0300

    Update src/index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ module.exports = function check(str, bracketsConfig) {
     let tempStr = str;
 
     for (let strIndex = tempStr.length; strIndex >= 0; strIndex--) {
-        for (let bracketIndex  = 0; bracketIndex < bracketsConfig.length; bracketIndex++) {
+        for (let bracketIndex = 0; bracketIndex < bracketsConfig.length; bracketIndex++) {
             if (tempStr[strIndex] === bracketsConfig[bracketIndex][0]) {
                 tempStr = tempStr.replace(bracketsConfig[bracketIndex].join(''), '');
             }
